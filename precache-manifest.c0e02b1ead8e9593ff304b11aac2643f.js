15890641a920ba5673dc7bebc88fbe4ba1ef8abc
tamaspeto1234 <tamas.peto.jobs@gmail.com>
2020-12-16 20:22:50 +0100

    Updates

---
+++ precache-manifest.c0e02b1ead8e9593ff304b11aac2643f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cbec0ffa5b7b444498af",
+    "url": "/glow/static/css/main.6efef3aa.chunk.css"
+  },
+  {
+    "revision": "cbec0ffa5b7b444498af",
+    "url": "/glow/static/js/main.cbec0ffa.chunk.js"
+  },
+  {
+    "revision": "a6f3a2216de1c9165847",
+    "url": "/glow/static/js/1.a6f3a221.chunk.js"
+  },
+  {
+    "revision": "9942df8a9dcabb5a48b2",
+    "url": "/glow/static/js/runtime~main.9942df8a.js"
+  },
+  {
+    "revision": "72c43337f6f7556edb84f772e09cca82",
+    "url": "/glow/index.html"
+  }
+];
